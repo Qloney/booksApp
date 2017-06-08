98de8990eb12230b068184ae6f04b376203b9a18
Архипенко Игорь <i.arkhipenko@domain.local>
2017-06-08 16:45:18 +0300

    Conditional rendering

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function() {
   return [
-    {title: 'Learnhing React'},
-    {title: 'Learning React with Redux'},
-    {title: 'Learning Webpack'},
-    {title: 'Learning UI-materialize'},
-    {title: 'Learning Hype technology'},
+    {title: 'Learnhing React', pages: 101},
+    {title: 'Learning React with Redux', pages: 51},
+    {title: 'Learning Webpack', pages: 21},
+    {title: 'Learning UI-materialize', pages: 17},
+    {title: 'Learning Hype technology', pages: 51},
   ];
 }
