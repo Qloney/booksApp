e5554e5baa004c9f99eae5cf0e0c619b9e8d9c30
Архипенко Игорь <i.arkhipenko@domain.local>
2017-06-08 10:40:53 +0300

    + Reducers

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    {title: 'Learnhing React'},
+    {title: 'Learning React with Redux'},
+    {title: 'Learning Webpack'},
+    {title: 'Learning UI-materialize'},
+    {title: 'Learning Hupe technology'},
+  ]
+}
